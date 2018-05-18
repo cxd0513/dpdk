610fef17240a0c2eb49711d05f9bab9ba0e81b10
cxd0513 <caoxudong_sx@qiyi.com>
2018-05-18 15:03:15 +0800

    ssh1

--- script.sh
+++ script.sh
@@ -1,4 +1,4 @@
-cd dpdk/dpdk-stable-17.05.2
+cd dpdk-stable-17.05.2
 sed -i "s/pci_intx_mask_supported(dev)/pci_intx_mask_supported(dev)||true/g" lib/librte_eal/linuxapp/igb_uio/igb_uio.c
 patch -p 1 < 0001-PATCH-kni-use-netlink-event-for-multicast-driver-par.patch
 make config T=x86_64-native-linuxapp-gcc
